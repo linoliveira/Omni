c95db718cbd0f60e2488c7de824eac12a0e865c6
Maria <maria.aumann@student.tugraz.at>
2015-01-09 14:45:46 +0000

    Revert "xy"
    This reverts commit 9acc218daf01267fbb4206e52fd9ee9cc083c0df.

--- source/views/restaurant_screen.js
+++ source/views/restaurant_screen.js
@@ -1,248 +1,132 @@
 enyo.kind(
-		{name: "restaurant_screen", kind: "FittableRows", classes: "moon enyo-unselectable enyo-fit full",
-			style: "background-image: url(\"assets/food_services/background.png\");",
-			components:
+{name: "restaurant_screen", kind: "FittableRows", classes: "moon enyo-unselectable enyo-fit full", fit:true,
+	components:
 		[
-{content: "Restaurant Services", style: "font-size: 60px; margin-left: 35px; color: white;"},
-	{kind: "FittableColumns", fit: true,
-		style: "padding: 0px; margin: 0px;",
-		components:
-			[
-			 	{kind: "FittableRows", style: "padding: 10px;",
-			 		components:
-			 			[
-			 			 	{kind: "FittableColumns", style:"background-color:rgba(0, 0, 0, 0.4); margin: 20px; margin-left: 10px; margin-bottom: 30px; padding-right: 15px;",
-			 			 		components:
-			 			 		[
-								{content: "Starters", style: "padding-top: 60px; color: white;"}
-			 			 		]
-			 			 	},
-			 			 	{name: "requestStartersContainer", kind: "FittableRows",
-			 			 		components:
-			 			 			[
-			 			 			 	// requestExtraComponents
-			 			 			]
-			 			 	}
-			 			]
-			 	},
-			 	{kind: "FittableRows", style: "padding: 10px;",
-			 		components:
-			 			[
-			 			 	{kind: "FittableColumns", style:"background-color:rgba(0, 0, 0, 0.4); margin: 20px; margin-bottom: 30px; padding-right: 50px;",
-			 			 		components:
-			 			 		[
-						 		 {content: "Main Courses", style: "padding-top: 60px; color: white;"}
-			 			 		]
-			 			 	},
-			 			 	{name: "requestMCContainer", kind: "FittableRows",
-			 			 		components:
-			 			 			[
-			 			 			 	// MCComponents
-			 			 			]
-			 			 	}
-			 			]
-			 	},
-			 	{kind: "FittableRows", style:"padding-bottom: 10px;",
-			 		components:
-			 			[
-			 			 	{kind: "FittableColumns", style:"background-color:rgba(0, 0, 0, 0.4); margin: 30px; padding-right: 10px;",
-			 			 		components:
-			 			 		[
-			 			 		 	{content: "Deserts", style: "padding-top: 60px; color: white;"}
-			 			 		]
-			 			 	},
-			 			 	{name: "requestDesertContainer", kind: "FittableRows",
-			 			 		components:
-			 			 			[
-			 			 			 	// requestExtraComponents
-			 			 			]
-			 			 	}
-			 			]
-			 	},
-			 	{kind: "FittableRows", style:"padding-bottom: 10px; margin: 20px;",
-			 		components:
-			 			[
-			 			 	{kind: "FittableColumns", style:"background-color:rgba(0, 0, 0, 0.4); margin-bottom: 30px; margin-top: 30px; margin-left: 10px; padding-right: 10px",
-			 			 		components:
-			 			 		[
-			 			 		 	{content: "Drinks", style: "margin-left: 10px; padding-top: 60px; color: white;"}
-			 			 		]
-			 			 	},
-			 			 	{name: "requestDrinkContainer", kind: "FittableRows",
-			 			 		components:
-			 			 			[
-			 			 			 	// requestExtraComponents
-			 			 			]
-			 			 	}
-			 			]
-			 	}
-			 	
-			
-			]
-	},
-	{components:
- 		[
-		 	{kind: "moon.Button", content: "Cancel Order", style: "margin: 50px;", ontap: "onRestaurantCancelTapped"}
-		]
-	}
-],
-
-//Restaurant Cancel Order
-onRestaurantCancelTapped: function()
-{
-	this.owner.loadMainScreen();
-},
-// Screen Initialization
-rendered: function()
-{
-    this.inherited(arguments);
-    this.requestContainersTapped();
-},
-
-//Room Services Request Starters Header Tapped
-requestContainersTapped: function(inSender, inEvent)
-{
-	//console.log(this.owner.jQuery);
-	var dis = this;
-	
-	var a = this.getContainers();
-	a.forEach(function(entry) {
-	    //console.log(this);
-		dis.createNewRequest(entry.id, entry.name);
-	});
-},
-
-getContainers: function()
-{
-	var obj = null;
-	
-	try
-	{
-		obj = this.webService("foodmenu/");
-	}
-	catch(e)
-	{
-		console.log(e);
-	}
-	
-	return obj;
-},
-
-
-
-createNewRequest: function(id, name)
-{
-	imageName = "";
-	switch(id)
-	{
-		case 1:
-			{
-			imageName = "main_2.png";
-			this.$.requestMCContainer.createComponent(
-					{kind: "moon.Item",  ontap: "requestTapped", popup: "requestPopup",
-						serviceID: id, serviceName: name,
-						style: "margin-left: 10px; padding: 0px; margin-top: 20px; width: 424px; height: 100px; background-image: url(\"assets/room_services/panels/request_aditional_panel.png\"); background-repeat: no-repeat; background-size: auto;",
-						components:
-		 			 		[
-				 			 	{kind: "FittableColumns", style: "padding: 0px; margin: 0px;",
-				 			 		components:
-				 			 		[
-										{kind: "enyo.Image", src: "assets/food_services/images/" + imageName},
-										{content: name, style: "padding-top: 35px; padding-left: 20px; color: white;"}
-				 			 		]
-				 			 	}
-		 			 		]
-			 		}, {owner: this}
-				).render();
-				break;
-				};
-		case 2:
+		 	{content: "Restaurant Services", style: "font-size: 40px;"},
+		 	{kind: "FittableColumns", style: "padding-left: 50px;", fit:true,
+		 		components:
+		 			[
+		 			 	{
+		 			 		components:
+		 			 			[
+									{content: "Starters"},
+									{kind: "moon.Scroller", style: "padding: 10px;", spotlight: "container", components:
+										[
+											{kind: "enyo.Repeater", count:"4", //onSetupItem: "setImageSource",
+													components:
+													[
+														{kind: "moon.Item", style:"padding-bottom: 2px; border-radius: 7px;", ontap: "onStarterTapped",
+																components:
+																[
+															 		 	{kind: "enyo.Image", src: "http://placehold.it/400x100/505050/ffffff&text=Starter"}
+																]
+														},
+													]
+											}
+										]
+									},
+		 			 			 ]
+		 			 	},
+		 			 	{
+		 			 		components:
+		 			 			[
+									{content: "Main Course"},
+									{kind: "moon.Scroller", style: "padding: 10px;", spotlight: "container", components:
+										[
+											{kind: "enyo.Repeater", count:"3", //onSetupItem: "setImageSource",
+													components:
+													[
+														{kind: "moon.Item", style:"padding-bottom: 2px; border-radius: 7px;", ontap: "onCourseTapped",
+																components:
+																[
+															 		 	{kind: "enyo.Image", src: "http://placehold.it/400x120/505050/ffffff&text=Course"}
+																]
+														},
+													]
+											}
+										]
+									},
+		 			 			 ]
+		 			 	},
+		 			 	{
+		 			 		components:
+		 			 			[
+									{content: "Desert"},
+									{kind: "moon.Scroller", style: "padding: 10px;", spotlight: "container", components:
+										[
+											{kind: "enyo.Repeater", count:"2", //onSetupItem: "setImageSource",
+													components:
+													[
+														{kind: "moon.Item", style:"padding-bottom: 2px; border-radius: 7px;", ontap: "onDesertTapped",
+																components:
+																[
+															 		 	{kind: "enyo.Image", src: "http://placehold.it/400x225/505050/ffffff&text=Desert"}
+																]
+														},
+													]
+											}
+										]
+									},
+		 			 			 ]
+		 			 	},
+		 			 	{
+		 			 		components:
+		 			 			[
+									{content: "Drinks"},
+									{kind: "moon.Scroller", style: "padding: 10px;", spotlight: "container", components:
+										[
+											{kind: "enyo.Repeater", count:"1", //onSetupItem: "setImageSource",
+													components:
+													[
+														{kind: "moon.Item", style:"padding-bottom: 2px; border-radius: 7px;", ontap: "onDrinkTapped",
+																components:
+																[
+															 		 	{kind: "enyo.Image", src: "http://placehold.it/400x225/505050/ffffff&text=Drink"}
+																]
+														},
+													]
+											}
+										]
+									},
+		 			 			 ]
+		 			 	}
+		 			]
+		 	},
+		 	{components:
+		 		[
+		 		 	{kind: "moon.Button", content: "Cancel Order", style: "margin: 50px;", ontap: "onRestaurantCancelTapped"}
+		 		]
+		 	}
+		],
+		
+		// Restaurant Cancel Order
+		onRestaurantCancelTapped: function()
 		{
-		imageName = "starters_2.png";
-		this.$.requestStartersContainer.createComponent(
-				{kind: "moon.Item", ontap: "requestTapped", popup: "requestPopup",
-					serviceID: id, serviceName: name,
-					style: "margin-left: 10px; padding: 0px; margin-top: 20px; width: 424px; height: 100px; background-image: url(\"assets/room_services/panels/request_aditional_panel.png\"); background-repeat: no-repeat; background-size: auto;",
-					components:
-	 			 		[
-			 			 	{kind: "FittableColumns", style: "padding: 0px; margin: 0px;",
-			 			 		components:
-			 			 		[
-									{kind: "enyo.Image", src: "assets/food_services/images/" + imageName},
-									{content: name, style: "padding-top: 35px; padding-left: 20px; color: white;"}
-			 			 		]
-			 			 	}
-	 			 		]
-		 		}, {owner: this}
-			).render();
-			break;
-			};
-		case 4:
+			this.owner.loadMainScreen();
+		},
+		
+		// Restaurant Starter Tapped
+		onStarterTapped: function(inSender, inEvent)
 		{
-		imageName = "dessert_2.png";
-		this.$.requestDesertContainer.createComponent(
-				{kind: "moon.Item",  ontap: "requestTapped", popup: "requestPopup",
-					serviceID: id, serviceName: name,
-					style: "margin-left: 10px; padding: 0px; margin-top: 20px; width: 424px; height: 100px; background-image: url(\"assets/room_services/panels/request_aditional_panel.png\"); background-repeat: no-repeat; background-size: auto;",
-					components:
-	 			 		[
-			 			 	{kind: "FittableColumns", style: "padding: 0px; margin: 0px;",
-			 			 		components:
-			 			 		[
-									{kind: "enyo.Image", src: "assets/food_services/images/" + imageName},
-									{content: name, style: "padding-top: 35px; padding-left: 20px; color: white;"}
-			 			 		]
-			 			 	}
-	 			 		]
-		 		}, {owner: this}
-			).render();
-			break;
-			};
-		case 3:
+			this.owner.loadFoodInfoScreen();
+		},
+		
+		// Restaurant Course Tapped
+		onCourseTapped: function(inSender, inEvent)
 		{
-		imageName = "drink_2.png";
-		this.$.requestDrinkContainer.createComponent(
-				{kind: "moon.Item",  ontap: "requestTapped", popup: "requestPopup",
-					serviceID: id, serviceName: name,
-					style: "margin-left: 10px; padding: 0px; margin-top: 20px; width: 424px; height: 100px; background-image: url(\"assets/room_services/panels/request_aditional_panel.png\"); background-repeat: no-repeat; background-size: auto;",
-					components:
-	 			 		[
-			 			 	{kind: "FittableColumns", style: "padding: 0px; margin: 0px;",
-			 			 		components:
-			 			 		[
-									{kind: "enyo.Image", src: "assets/food_services/images/" + imageName},
-									{content: name, style: "padding-top: 35px; padding-left: 20px; color: white;"}
-			 			 		]
-			 			 	}
-	 			 		]
-		 		}, {owner: this}
-			).render();
-			break;
-			};
-			
-	}
-	
-	
-},
-
-//Room Services Request Extra Tapped
-requestTapped: function(inSender,inEvent)
-{	
-	var popUp = this.$[inSender.popup];
-	var serviceName = inSender.components[0].components[1].content;
-	var description = inSender.components[0].components[1].content;
-	var price = inSender.components[0].components[1].content;
-	var serviceID = inSender.serviceID;
-	this.owner.loadFoodInfoScreen();
-	return true;
-},
-
-webService: function(URL, data)
-{
-	return this.owner.webService(URL, data);
+			this.owner.loadFoodInfoScreen();
+		},
+		
+		// Restaurant Desert Tapped
+		onDesertTapped: function(inSender, inEvent)
+		{
+			this.owner.loadFoodInfoScreen();
+		},
+		
+		// Restaurant Drink Tapped
+		onDrinkTapped: function(inSender, inEvent)
+		{
+			this.owner.loadFoodInfoScreen();
+		},
 }
-
-
-		 			 	
-});
-		 
+);
